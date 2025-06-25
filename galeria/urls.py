0b1f2be3175e9697de052eef18771758743e30e3
Andre Nechio <kgsnechio@gmail.com>
2025-06-25 09:48:58 -0300

    artigo page

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, outro
+from galeria.views import index, outro, artigo
 
 urlpatterns = [
     path('', index, name='index'),
     path('outro/', outro, name='outro'),
+    path('artigo/<int:artigo_id>', artigo, name='artigo'),
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from galeria.models import ArtigoGaleria
@@ -6,12 +6,14 @@ from galeria.models import ArtigoGaleria
 def index(request):
 
     cards = ArtigoGaleria.objects.all()
-    print(cards)
 
     return render(request, 'galeria/index.html', { 'cards': cards })
 
+def artigo(request, artigo_id):
+
+    artigo = get_object_or_404(ArtigoGaleria, pk=artigo_id)
+
+    return render(request, 'galeria/artigo.html', { 'artigo': artigo })
 
 def outro(request):
-
     return render(request, 'galeria/outro.html')
-
