aaa6d6c2ff53c566fd535a9546247fa7021468fd
kgsnechio <kgsnechio@gmail.com>
2025-06-29 23:41:58 -0300

    home estudio

--- setup/urls.py
+++ setup/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('galeria.urls')),
     path('', include('usuarios.urls')),
+    path('', include('estudio.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
